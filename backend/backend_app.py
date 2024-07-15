b7efcb83026a4c980711ad1f4ac82013c787ef5f
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2024-07-15 12:02:43 +0200

    added API documentation

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -114,6 +115,19 @@ def search_posts():
     error_message = " or ".join(error_message_parts)
 
     return jsonify({"message": f"No posts found with the {error_message}"}), 404
+
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/blog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Blog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 
 if __name__ == '__main__':
